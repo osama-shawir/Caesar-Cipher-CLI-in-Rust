83e5347a4fcefc5100edd20666775418ddb9de5b
osama-shawir <89782802+osama-shawir@users.noreply.github.com>
2023-10-22 06:30:06 +0000

    adj

--- lowmem-fruit-salad/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use rand::seq::SliceRandom;
-use rand::thread_rng;
-
-pub fn create_fruit_salad(mut fruits: Vec<String>) -> Vec<String> {
-    let mut rng = thread_rng();
-    fruits.shuffle(&mut rng);
-
-    fruits
-}
